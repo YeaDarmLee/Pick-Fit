ceed09de2c3fe373679420915a58192b0c70c459
YeaDarmLee <gnswpwhrqkf3@gmail.com>
2021-09-09 11:49:13 +0900

    화면 전환 jQuery, css 추가

---
+++ static/js/main.js
@@ -0,0 +1,13 @@
+// 화면 전환
+jQuery(function($) {
+  $("body").css("display", "none");
+  $("body").fadeIn(800);
+  $("a.transition").click(function(event){
+    event.preventDefault();
+    linkLocation = this.href;
+    $("body").fadeOut(800, redirectPage);
+  });
+  function redirectPage() {
+    window.location = linkLocation;
+  }
+});
