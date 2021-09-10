9ceee63b999f51a9b86384b8a22e47aa5f4dd772
YeaDarmLee <gnswpwhrqkf3@gmail.com>
2021-09-10 18:53:36 +0900

    [크롤링] DB 로직 변경

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def crawler():
     result_txt = []
     result_img = []
     # 크롤링
-    # soup.select_one('#s_content > div.section > ul > li:nth-child(1) > dl > dt > a')
+    # soup.select_one('//*[@id="tt-body-page"]/script[2]')
     for k in range(0, len(tag)):
       # 태그 Null 값 제외
       if (tag[k] != ''):
@@ -78,7 +78,7 @@ def crawler():
             result_txt.append(soup.find(tag[k]).get_text())
           else:
             result_txt.append(soup.find(tag[k],class_=classNm[k]).get_text())
-    
+
     # return 형식 저장
     result = {
       "code": 20000,
@@ -88,7 +88,8 @@ def crawler():
       'result_img': result_img,
       'result_txt': result_txt
     }
-  except:
+  except Exception as e:
+    print(e)
     result = {
       "code": 40000,
       "url": '',
@@ -105,35 +106,42 @@ def crawler():
 def addTample():
   # front에서 넘겨받은 form값
   rq_form = request.form
+  print(rq_form)
 
-  tampleName = request.form.get('tampleName')
-  tampleUrl = request.form.get('tampleUrl')
+  hostName = request.form.get('hostName')
+  url = request.form.get('url')
+  detail_url = request.form.get('detail_url')
   
-  # URL 입력시 자동 인코딩으로 인한 TypeError로 디코딩 처리
-  tampleUrl = parse.unquote(tampleUrl)
+  url = parse.unquote(url)
+  detail_url = parse.unquote(detail_url)
 
-  j = 0
-  tag = []
-  classNm = []
-  # 크롤링에 필요한 tag와 class 값 분할
-  for i in rq_form:
-    if (i != 'tampleName' and i != 'tampleUrl'):
-      if (j != 0 and j%2 == 0):
-        tag.append(request.form.get(i))
-      else:
-        classNm.append(request.form.get(i))
-    j=j+1
-  
-  print('tag',tag)
-  print('classNm',classNm)
+  price = request.form.get('price')
+  p_name = request.form.get('p_name')
+  width = request.form.get('width')
+  flexibility = request.form.get('flexibility')
+  seethrough = request.form.get('seethrough')
+  lining = request.form.get('lining')
+  material = request.form.get('material')
+  p_img = request.form.get('p_img')
+  size = request.form.get('size')
+  color = request.form.get('color')
 
-  # data insert
   try:
     db_class = dbModule.Database()
-    sql = "INSERT INTO crawling_form (title,url,meta_tag,meta_class) VALUES ('" + tampleName + "','" + str(tampleUrl) + "',\"" + str(tag) + "\",\"" + str(classNm) + "\")"
-    print (sql)
 
-    db_class.execute(sql)
+    # 현재 두 테이블간 의존성이 없이 동시에 insert하고 있어 둘중 하나가 에러나면 idx 번호 꼬임
+    # 이문제 처리해야하는데 귀찬음...
+
+    # url_info insert
+    sql_url_info = "INSERT INTO url_info (hostName,url,detail_url) VALUES ('" + hostName + "','" + str(url) + "',\"" + str(detail_url) + "\")"
+    print(sql_url_info)
+    db_class.execute(sql_url_info)
+
+    # product_info insert
+    sql_product_info = "INSERT INTO product_info (price,p_name,width,flexibility,seethrough,lining,material,p_img,size,color) VALUES ('" + str(price) + "','" + str(p_name) + "','" + str(width) + "','" + str(flexibility) + "','" + str(seethrough) + "','" + str(lining) + "','" + str(material) + "','" + str(p_img) + "','" + str(size) + "','" + str(color) + "')"
+    print(sql_product_info)
+    db_class.execute(sql_product_info)
+
     db_class.commit()
     result = {
       "code": 20000
@@ -152,11 +160,11 @@ def addTample():
 def searchTample():
 
   db_class = dbModule.Database()
-  sql = "SELECT * FROM crawling_form;"
-  row = db_class.executeAll(sql)
+  search_url_info = "SELECT * FROM url_info;"
+  url_info_result = db_class.executeAll(search_url_info)
 
   result = []
-  for i in row:
+  for i in url_info_result:
     result.append(i)
 
   return {'result': result}
@@ -168,14 +176,25 @@ def searchTample():
 # test code
 @app.route('/test',methods=('GET', 'POST'))
 def test():
-  url = 'https://wjdqnwnd.cafe24.com/product/detail.html?product_no=7132&cate_no=99&display_group=1'
-  headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'}
-  response = requests.get(url, headers=headers).text
-  html_text = response
-  soup = BeautifulSoup(html_text, "html.parser")
-  print(soup)
-  test = soup.find('h2')
-  print(test)
+  from selenium import webdriver
+  from webdriver_manager.chrome import ChromeDriverManager
+
+  options = webdriver.ChromeOptions()
+  options.add_argument('headless')
+  options.add_argument('window-size=1920x1080')
+  options.add_argument("disable-gpu")
+
+  # driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
+  driver = webdriver.Chrome("chromedriver.exe", chrome_options=options)
+
+  driver.get('https://mr-s.co.kr/product/detail.html?product_no=40244')
+  driver.implicitly_wait(2)
+  # driver.get_screenshot_as_file('test.png')
+
+  print('find_elements_by_xpath : ',[e.text for e in driver.find_elements_by_xpath('/html/body/div[2]/div[3]/div[4]/div[1]/div/center[2]/div[1]/div[2]/div[2]/div[2]')])
+
+  
+  driver.close()
 
   return 'test'
 
--- sql/PICK_FIT.sql
+++ sql/PICK_FIT.sql
@@ -10,3 +10,55 @@ create table crawling_form(
     c_date datetime NOT NULL DEFAULT current_timestamp,
     PRIMARY KEY(id)
 );
+
+-- 크롤링할 url 정보 테이블
+create table url_info(
+	idx INT NOT NULL AUTO_INCREMENT,
+	hostName VARCHAR(500),
+	url VARCHAR(1000),
+	detail_url VARCHAR(1000),
+    c_date datetime NOT NULL DEFAULT current_timestamp,
+    PRIMARY KEY(idx)
+);
+
+-- 크롤링할 상품 정보 테이블
+create table product_info(
+	idx INT NOT NULL AUTO_INCREMENT,
+    price VARCHAR(100),
+    p_name VARCHAR(500),
+    width VARCHAR(50),
+	flexibility VARCHAR(50),
+	seethrough VARCHAR(50),
+	lining VARCHAR(50),
+	material VARCHAR(50),
+	p_img VARCHAR(1000),
+	size VARCHAR(500),
+	color VARCHAR(500),
+    PRIMARY KEY(idx)
+);
+
+-- 크롤링 url 결과
+create table crawling_url(
+	idx INT NOT NULL AUTO_INCREMENT,
+	hostName VARCHAR(500),
+	url VARCHAR(1000),
+	detail_url VARCHAR(1000),
+    c_date datetime NOT NULL DEFAULT current_timestamp,
+    PRIMARY KEY(idx)
+);
+
+-- 크롤링 product 결과
+create table crawling_product(
+	idx INT NOT NULL AUTO_INCREMENT,
+    price VARCHAR(100),
+    p_name VARCHAR(500),
+    width VARCHAR(50),
+	flexibility VARCHAR(50),
+	seethrough VARCHAR(50),
+	lining VARCHAR(50),
+	material VARCHAR(50),
+	p_img VARCHAR(1000),
+	size VARCHAR(500),
+	color VARCHAR(500),
+    PRIMARY KEY(idx)
+);
