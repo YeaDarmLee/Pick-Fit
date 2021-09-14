e8f494aec5a629c4252e104e357621b791153688
YeaDarmLee <gnswpwhrqkf3@gmail.com>
2021-09-14 18:55:35 +0900

    Update PICK_FIT.sql

--- application/sql/PICK_FIT.sql
+++ application/sql/PICK_FIT.sql
@@ -1,16 +1,5 @@
 CREATE DATABASE PICK_FIT;
 
--- 크롤링 form 저장 테이블
-create table crawling_form(
-	id INT NOT NULL AUTO_INCREMENT,
-	title VARCHAR(500),
-	url VARCHAR(1000),
-    meta_tag VARCHAR(2000),
-    meta_class VARCHAR(2000),
-    c_date datetime NOT NULL DEFAULT current_timestamp,
-    PRIMARY KEY(id)
-);
-
 -- 크롤링할 url 정보 테이블
 create table url_info(
 	idx INT NOT NULL AUTO_INCREMENT,
