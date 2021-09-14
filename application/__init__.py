bdad16e51d72c63b0ca7e4244f3fcfc6354430d5
YeaDarmLee <gnswpwhrqkf3@gmail.com>
2021-09-14 16:47:42 +0900

    Update __init__.py

--- application/__init__.py
+++ application/__init__.py
@@ -3,10 +3,14 @@ from . import main
 from . import join
 from . import login
 from . import crawler
+from . import contact
 
 app = Flask(__name__)
+
+app.secret_key="0d54679e-1522-11ec-82a8-0242ac130003"
 
 app.register_blueprint(main.main)
 app.register_blueprint(join.join)
 app.register_blueprint(login.login)
 app.register_blueprint(crawler.crawler)
+app.register_blueprint(contact.contact)
