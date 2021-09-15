ad9109825f4f1129cffd7dabce2bfd2c02a3b254
YeaDarmLee <gnswpwhrqkf3@gmail.com>
2021-09-15 14:23:45 +0900

    login / join 매핑 변경

--- application/__init__.py
+++ application/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from . import main
-from . import join
-from . import login
+from . import user
 from . import crawler
 from . import contact
 from . import recommend
@@ -12,8 +11,7 @@ app = Flask(__name__)
 app.secret_key="0d54679e-1522-11ec-82a8-0242ac130003"
 
 app.register_blueprint(main.main)
-app.register_blueprint(join.join)
-app.register_blueprint(login.login)
+app.register_blueprint(user.user)
 app.register_blueprint(crawler.crawler)
 app.register_blueprint(contact.contact)
 app.register_blueprint(recommend.recommend)
