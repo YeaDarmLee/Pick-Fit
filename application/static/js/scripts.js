034f27138f5b4beb14a900a8f495e909fc2226b7
YeaDarmLee <gnswpwhrqkf3@gmail.com>
2021-09-16 17:58:50 +0900

    [loading] 페이지 전환시 기본값 0.5초 딜레이 추가

--- application/static/js/scripts.js
+++ application/static/js/scripts.js
@@ -59,16 +59,23 @@ window.addEventListener('DOMContentLoaded', event => {
 state = 상태 icon = "warning" "error" "success" "info"
 비동기 처리 위해 funType, url 받아와 사용
 */
-function alertStart(state, title, message, funType, text) {
+function alertStart(state, title, message, funType, text, time) {
   Swal.fire({
     icon: state,
     title: title,
     text: message,
   }).then(function(){
     if (funType == 'replace') {
-      location.replace(text)
+      change_url(text, time)
     } else if (funType == 'click') {
       $(text).click()
     }
   });
 };
+
+function change_url(url, time) {
+  $('#load').slideToggle()
+  setTimeout(function () {
+    location.replace(url)
+  }, time);
+}
